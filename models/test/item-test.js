5ff17f3b86126fb1b0efc8475f5b9d3c9778899e
Shawn Nolan <shawn@shawnnolan.com>
2018-03-12 23:49:02 -0700

    - Refactored Item model test. - Added item schema. - Updated package json to handle test directories.

--- models/test/item-test.js
+++ models/test/item-test.js
@@ -1,70 +1,66 @@
-let app = require('../../app');
-let assert = require('assert');
+"use strict";
+
+require('../../app');
 let mongoose = require('mongoose');
+let chai = require('chai');
+let expect = chai.expect;
 let dbConfig = require('../../db.config');
-let url = dbConfig.mongoConfigs.db.uri;
-let dbName = dbConfig.mongoConfigs.db.name;
-let dbCollection = dbConfig.mongoConfigs.db.collection;
-const Schema = mongoose.Schema;
-const chai = require('chai');
-const expect = chai.expect;
+let uri = dbConfig.mongoConfigs.db.uri;
 
 // Create a new schema that accepts a 'name' object.
-// 'name' is a required field
-const testSchema = new Schema({
-	name: { type: String, required: true }
-});
+const testInventory = require('../../schemas/itemSchema');
 
-//Create a new collection called 'Item'
-const Item = mongoose.model('Item', testSchema);
+//Create a new collection called 'item'
+const item = mongoose.model('item', testInventory);
 
-describe('Testing mongodb', function() {
+describe('Test item model', function() {
+	//Before starting the test, create a sandboxed database connection
+	//Once a connection is established invoke done()
 	before(function (done) {
-		mongoose.connect(url + dbName);
+		mongoose.connect(uri + 'testDatabase');
 		const db = mongoose.connection;
 		db.on('error', console.error.bind(console, 'connection error'));
 		db.once('open', function() {
-			console.log('We are connected to test database!');
 			done();
 		});
 	});
 	
-	describe('Test Find Item', function() {
-		it('Should retrieve data from inventory database', function(done) {
-			//Look up the 'Mike' object previously saved.
-			const collection = db.collection(dbCollection);
-			Item.find({_id: '5a98c2b97cb6d9d891980dcf'}, (err, name) => {
-				if(err) {throw err;}
-				if(name.length === 0) {throw new Error('No data!');}
-				done();
+	describe('test item actions', function() {
+		//Save object with required params
+		it('Save new item to db', function(done) {
+			let testItem = item({
+				item: 'journal',
+				qty: 25,
+				status: 'A',
+				size: {
+					"h" : 14,
+					"w" : 21,
+					"uom" : "cm"
+				}
+			});
+			testItem.save(done);
+		});
+		
+		it('Should NOT save to db with incorrect data and params', function(done) {
+			//Attempt to save with wrong info. An error should trigger
+			let wrongSave = item({ notName: 'Ryder' });
+			wrongSave.save(err => {
+				if(err) { return done(); }
+				throw new Error('Should generate error!');
 			});
 		});
-		//Save object with 'name' value of 'Mike"
-		// it('New name saved to test database', function(done) {
-		// 	let testName = Item({
-		// 		name: 'Mike'
-		// 	});
-		//
-		// 	testName.save(done);
-		// });
-		// it('Dont save incorrect format to database', function(done) {
-		// 	//Attempt to save with wrong info. An error should trigger
-		// 	let wrongSave = Item({
-		// 		notName: 'Not Mike'
-		// 	});
-		// 	wrongSave.save(err => {
-		// 		if(err) { return done(); }
-		// 		throw new Error('Should generate error!');
-		// 	});
-		// });
-		// it('Should retrieve data from test database', function(done) {
-		// 	//Look up the 'Mike' object previously saved.
-		// 	Item.find({name: 'Mike'}, (err, name) => {
-		// 		if(err) {throw err;}
-		// 		if(name.length === 0) {throw new Error('No data!');}
-		// 		done();
-		// 	});
-		// });
+		
+		it('Should get find item with param', function(done) {
+			item.find({ item: 'journal'}, (err, resp) => {
+				if(err) {throw err;}
+				if(resp.length === 0) {throw new Error('No data!');}
+				setTimeout(function(){
+					expect(resp[0]._doc.item).to.equal('journal');
+					expect(resp).to.be.an.instanceOf(Object);
+					done();
+				});
+			});
+		});
 	});
 	
 	//After all tests are finished drop database and close connection
---
+++ schemas/itemSchema.js
@@ -0,0 +1,30 @@
+'use strict';
+
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+require('../app');
+
+const item = new Schema({
+	item: {
+		type: String,
+		required: true
+	},
+	qty: {
+		type: Number,
+		required: true
+	},
+	status: {
+		type: String,
+		required: true
+	},
+	size: {
+		type: Object,
+		required: true
+	},
+	tags: {
+		type: Array,
+		required: true
+	}
+});
+
+module.exports = item;
